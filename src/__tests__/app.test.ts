0df93e29e4e3aabd0ecb851697ccd1b9895fab6d
HungryHippo8809 <109061468+HungryHippo8809@users.noreply.github.com>
2022-08-18 17:05:33 +0900

    Fail Test Code 제외

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -7,15 +7,15 @@ test('페이지 로드 시 SplitPane이 생성되어 있어야 함', () => {
   expect(results.getAllByRole('dialog')).toBeTruthy()
 })
 
-test('Window Add Fail Test', async () => {
-  const results = render(App)
+// test('Window Add Fail Test', async () => {
+//   const results = render(App)
 
-  const addBtn = results.getByTestId('btnAddTest')
-  const beforeCount = results.getAllByRole('dialog').length
-  await userEvent.click(addBtn)
-  const AfterCount = results.getAllByRole('dialog').length
-  expect(beforeCount).toEqual(AfterCount) // 버튼을 클릭 하였으나 기존과 동일한 Count라면 Fail
-})
+//   const addBtn = results.getByTestId('btnAddTest')
+//   const beforeCount = results.getAllByRole('dialog').length
+//   await userEvent.click(addBtn)
+//   const AfterCount = results.getAllByRole('dialog').length
+//   expect(beforeCount).toEqual(AfterCount) // 버튼을 클릭 하였으나 기존과 동일한 Count라면 Fail
+// })
 
 test('Window Add Test', async () => {
   const results = render(App)
