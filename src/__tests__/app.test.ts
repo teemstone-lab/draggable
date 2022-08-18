fe15f56804146329bb10007930795155779bf53a
HungryHippo8809 <109061468+HungryHippo8809@users.noreply.github.com>
2022-08-18 15:41:51 +0900

    Add Commit

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,10 +1,30 @@
-// import userEvent from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 import { render } from '@testing-library/svelte'
 import App from '../App.svelte'
 
 test('페이지 로드 시 SplitPane이 생성되어 있어야 함', () => {
   const results = render(App)
   expect(results.getAllByRole('dialog')).toBeTruthy()
+})
+
+test('Window Add Fail Test', async () => {
+  const results = render(App)
+
+  const addBtn = results.getByTestId('btnAddTest')
+  const beforeCount = results.getAllByRole('dialog').length
+  await userEvent.click(addBtn)
+  const AfterCount = results.getAllByRole('dialog').length
+  expect(beforeCount).toEqual(AfterCount) // 버튼을 클릭 하였으나 기존과 동일한 Count라면 Fail
+})
+
+test('Window Add Test', async () => {
+  const results = render(App)
+
+  const addBtn = results.getByTestId('btnAddTest')
+  const beforeCount = results.getAllByRole('dialog').length
+  await userEvent.click(addBtn)
+  const AfterCount = results.getAllByRole('dialog').length
+  expect(beforeCount + 1).toEqual(AfterCount)
 })
 
 // test('Window Add Fail Test', async () => {
