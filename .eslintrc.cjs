a42e987704c6e564f84d9a6c34f20f800ad2a96d
amanokaze <oniamano@gmail.com>
2022-08-31 16:08:54 +0900

    Github Page Apply

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
       'plugin:@typescript-eslint/recommended',
       'plugin:@typescript-eslint/recommended-requiring-type-checking'
    ],
-   ignorePatterns: ['docs/','.eslintrc.*', 'vite.config.*', 'svelte.config.*'],
+   ignorePatterns: ['assets/','.eslintrc.*', 'vite.config.*', 'svelte.config.*'],
    parser: '@typescript-eslint/parser',
    parserOptions: {
       ecmaVersion: 13,
